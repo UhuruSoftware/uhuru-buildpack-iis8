d4dd77d4d4f05c5d677c8bed719497032526bc17
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2014-11-18 17:36:39 +0200

    Add stop handler
    Disable the buffer for printf
    Format source code with VS

--- iishwc/src/iishwc/iishwc.cpp
+++ iishwc/src/iishwc/iishwc.cpp
@@ -7,9 +7,31 @@
 // Set the project's calling convention to "__stdcall (/Gz)".
 // Use Windows Events or IPC communication with the DEA http://msdn.microsoft.com/en-us/library/System.Threading.EventWaitHandle.aspx
 
+
+HANDLE hStopEvent;
+
+BOOL CtrlHandler(DWORD fdwCtrlType)
+{
+	switch (fdwCtrlType)
+	{
+		// Handle the CTRL-C signal.
+	case CTRL_C_EVENT:
+		SetEvent(hStopEvent);
+		return(TRUE);
+
+	default:
+		return FALSE;
+	}
+}
+
+
+
 // First argument is the path to the application host config.
 HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
 	// Create a handle for the Web core DLL.
 	HINSTANCE hDLL;
 
@@ -24,7 +46,7 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 	WCHAR wszInstanceName[30];
 
 	// Retrieve the path of the Inetsrv folder.
-	DWORD nSize = ::ExpandEnvironmentStringsW(L"%windir%\\system32\\inetsrv",wszInetPath,MAX_PATH);
+	DWORD nSize = ::ExpandEnvironmentStringsW(L"%windir%\\system32\\inetsrv", wszInetPath, MAX_PATH);
 
 	// Exit if the path of the Inetsrv folder cannot be determined.
 	if (nSize == 0)
@@ -33,32 +55,35 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 		hr = HRESULT_FROM_WIN32(GetLastError());
 		// Return an error status to the console.
 		printf("Could not determine the path to the Inetsrv folder.\n");
-		printf("Error: 0x%x\n",hr);
+		printf("Error: 0x%x\n", hr);
 		// Return an error from the application and exit.
 		return hr;
 	}
 
 	// Append the Web core DLL name to the Inetsrv path.
-	wcscpy_s(wszDllPath,MAX_PATH-1,wszInetPath);
-	wcscat_s(wszDllPath,MAX_PATH-1,L"\\");
-	wcscat_s(wszDllPath,MAX_PATH-1,WEB_CORE_DLL_NAME);
+	wcscpy_s(wszDllPath, MAX_PATH - 1, wszInetPath);
+	wcscat_s(wszDllPath, MAX_PATH - 1, L"\\");
+	wcscat_s(wszDllPath, MAX_PATH - 1, WEB_CORE_DLL_NAME);
 
 	if (argc == 4)
 	{
 		// Use the first argument from the run command as the config file path
-		wcscpy_s(wszCfgPath,MAX_PATH-1,argv[1]);
+		wcscpy_s(wszCfgPath, MAX_PATH - 1, argv[1]);
 
 		// Use the second argument from the run command as the root web config file path
-		wcscpy_s(wszRootWebCfgPath,MAX_PATH-1,argv[2]);
+		wcscpy_s(wszRootWebCfgPath, MAX_PATH - 1, argv[2]);
 
 		// Use the third argument as the name of the web core instance
-		wcscpy_s(wszInstanceName,29,argv[3]);
+		wcscpy_s(wszInstanceName, 29, argv[3]);
 	}
 	else
 	{
 		printf("Invalid parameters, use the following: iishwc [applicationHost.config] [rootweb.config] [instance-id]");
 		return 1;
 	}
+
+	hStopEvent = CreateEvent(NULL, true, false, NULL);
+	SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
 
 	// Create a pointer to WebCoreActivate.
 	PFN_WEB_CORE_ACTIVATE pfnWebCoreActivate = NULL;
@@ -76,7 +101,7 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 		hr = HRESULT_FROM_WIN32(GetLastError());
 		// Return an error status to the console.
 		printf("Could not load DLL.\n");
-		printf("Error: 0x%x\n",hr);
+		printf("Error: 0x%x\n", hr);
 	}
 	else
 	{
@@ -84,15 +109,15 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 		printf("DLL loaded successfully.\n");
 		// Retrieve the address for "WebCoreActivate".
 		pfnWebCoreActivate = (PFN_WEB_CORE_ACTIVATE)GetProcAddress(
-			hDLL,"WebCoreActivate");
+			hDLL, "WebCoreActivate");
 		// Test for an error.
-		if (pfnWebCoreActivate==NULL)
+		if (pfnWebCoreActivate == NULL)
 		{
 			// Retrieve the last error.
 			hr = HRESULT_FROM_WIN32(GetLastError());
 			// Return an error status to the console.
 			printf("Could not resolve WebCoreActivate.\n");
-			printf("Error: 0x%x\n",hr);
+			printf("Error: 0x%x\n", hr);
 		}
 		else
 		{
@@ -100,15 +125,15 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 			printf("WebCoreActivate successfully resolved.\n");
 			// Retrieve the address for "WebCoreShutdown".
 			pfnWebCoreShutdown = (PFN_WEB_CORE_SHUTDOWN)GetProcAddress(
-				hDLL,"WebCoreShutdown");
+				hDLL, "WebCoreShutdown");
 			// Test for an error.
-			if (pfnWebCoreShutdown==NULL)
+			if (pfnWebCoreShutdown == NULL)
 			{
 				// Retrieve the last error.
 				hr = HRESULT_FROM_WIN32(GetLastError());
 				// Return an error status to the console.
 				printf("Could not resolve WebCoreShutdown.\n");
-				printf("Error: 0x%x\n",hr);
+				printf("Error: 0x%x\n", hr);
 			}
 			else
 			{
@@ -123,16 +148,19 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 				{
 					// Return an error status to the console.
 					printf("WebCoreActivate failed.\n");
-					printf("Error: 0x%x\n",hr);
+					printf("Error: 0x%x\n", hr);
 				}
 				else
 				{
 					// Return a success status to the console.
 					printf("WebCoreActivate was successful.\n");
+
 					// Prompt the user to continue.
-					printf("Press any key to continue...\n");
-					// Wait for a key press.
-					int iKeyPress = _getch();
+					printf("Press Ctrl-C to stop.\n");
+
+					// Wait for stop event.
+					WaitForSingleObject(hStopEvent, INFINITE);
+
 					// Return a shutdown status to the console.
 					printf("Shutting down the Web core...\n");
 					// Shut down the Web core.
@@ -142,7 +170,7 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 					{
 						// Return an error status to the console.
 						printf("WebCoreShutdown failed.\n");
-						printf("Error: 0x%x\n",hr);
+						printf("Error: 0x%x\n", hr);
 					}
 					else
 					{
@@ -154,7 +182,7 @@ HRESULT _cdecl wmain(int argc, wchar_t * argv[])
 		}
 		// Release the DLL.
 		FreeLibrary(hDLL);
-	}    
+	}
 	// Return the application status.
 	return hr;
 }
